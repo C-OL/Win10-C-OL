f1c697eda0b9cb0bf47927de6005ed4cb95400da
C-OL <83933988+C-OL@users.noreply.github.com>
2021-05-09 23:11:09 -0400

    Update Win10-C-OL.cmd

--- Win10-C-OL.cmd
+++ Win10-C-OL.cmd
@@ -1,17 +1,13 @@
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
-::																								::
-::	Windows 10 C-OL - Custom Tweaks Collection Script											::
-::																								::
-::	This is a collection of tweaks that aim to fine tune a fresh install of Windows 10 keeping	::
-::	the default behavior of visual inputs providing a drastically overall performance boost		::
-::	and enhanced security.																		::
-::																								::
-::	Some tweaks are meant for the CURRENT_USER instead of using Group Policies to avoid 		::
-::	similar annoying messages such as: "Some settings are managed by your organization".		::
-::																								::
-::	It requires to Run as Administrator to work properly.										::
-::																								::
-::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::
+:: Windows 10 C-OL - Custom Tweaks Collection Script
+::
+:: This is a collection of tweaks that aim to fine tune a fresh install of Windows 10 keeping
+:: the default behavior of visual inputs providing a drastically overall performance boost and enhanced security.
+::
+:: Some tweaks are meant for the CURRENT_USER instead of using Group Policies to avoid similar annoying messages
+:: such as: "Some settings are managed by your organization".
+::
+:: It requires to Run as Administrator to work properly.
+::
 
 @ECHO OFF & COLOR 30
 SETLOCAL EnableDelayedExpansion
